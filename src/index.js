dee3bf88e63c40d144d36030e2bf9e8d472231b6
Miguel <mgarcia95951@gmail.com>
2019-01-17 14:23:08 -0600

    setup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+/* 
+
+Routes
+---------
+domain.com/ = redirect to root or login
+domain.com/app = root
+          /login
+          /register
+          /app/room/{roomId} = room channels and categories
+          /app/room/{roomId}/{channelId} = channel messages 
+*/
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
