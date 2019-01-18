4009abd2427eff7c2096702d3995eeba4568cd5e
Miguel <mgarcia95951@gmail.com>
2019-01-18 14:20:32 -0600

    use firebase to create a new user

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,2 +1,9 @@
 export const SET_USER = 'SET_USER';
 export const CLEAR_USER = 'CLEAR_USER';
+
+export const CREATE_NEW_USER = 'CREATE_NEW_USER';
+export const CREATE_NEW_USER_ERROR = 'CREATE_NEW_USER_ERROR';
+export const LOGIN = 'LOGIN';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+export const LOGOUT = 'LOGOUT';
+export const LOGOUT_ERROR = 'LOGOUT_ERROR';
