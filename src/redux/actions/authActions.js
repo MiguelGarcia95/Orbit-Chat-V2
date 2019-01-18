ca5b2beed206300d99f0526c0c6d20b7e70becd8
Miguel <mgarcia95951@gmail.com>
2019-01-18 15:57:28 -0600

    create a new user

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,12 +1,45 @@
-import * as actionTypes from './types';
-import { dispatch } from 'rxjs/internal/observable/pairs';
+import * as actionTypes from '../actions/types';
+
+import md5 from 'md5';
 
 export const createNewUser = user => {
   return (dispatch, getState, {getFirebase, getFirestore}) => {
     const firebase = getFirebase();
     const firestore = getFirestore();
 
-    console.log('firebase: ', firebase)
-    console.log('firestore: ', firestore)
+    // console.log('firebase: ', firebase)
+    // console.log('firestore: ', firestore)
+
+    firebase.auth()
+      .createUserWithEmailAndPassword(user.email, user.password)
+      .then(createdUser => {
+        //Update Firebase User
+        createdUser.user.updateProfile({
+          displayName: user.username,
+          photoURL: `http://gravatar.com/avatar/${md5(user.email)}?d=identicon`
+        })
+
+        //Create Firestore user
+        firestore.add('users', {
+          username: user.username,
+          avatar: `http://gravatar.com/avatar/${md5(user.email)}?d=identicon`,
+          uid: createdUser.user.uid,
+          emai: user.email
+        });
+      }).then(() => {
+        dispatch({
+          type: actionTypes.CREATE_NEW_USER,
+          payload: {
+
+          }
+        })
+      }).catch(err => {
+        dispatch({
+          type: actionTypes.CREATE_NEW_USER_ERROR,
+          payload: {
+
+          }
+        })
+      })
   }
 }
