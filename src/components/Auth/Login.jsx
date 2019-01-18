74ccd2fa282f58d535db23f6e7e1c5b59aedeb35
Miguel <mgarcia95951@gmail.com>
2019-01-18 11:12:30 -0600

    login page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
 
 class Login extends React.Component {
   state = {
@@ -8,7 +9,28 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div>Login</div>
+      <Grid textAlign='center' verticalAlign='middle' className='app'>
+        <Grid.Column width={6}>
+          <Header as='h1' color='black'>
+            Login to Orbit
+          </Header>
+          <Form size='large'>
+            <Segment raised >
+              <Form.Field>
+                <Form.Input 
+                name='email' 
+                type='email'
+                placeholder='Email'
+                />
+              </Form.Field>
+              <Form.Field>
+                <Form.Input name='password' type='password' />
+              </Form.Field>
+            </Segment>
+          </Form>
+          <Message>Not a user? <Link to='/register'>Register</Link></Message>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
