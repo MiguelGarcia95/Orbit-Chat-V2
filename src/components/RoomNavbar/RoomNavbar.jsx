f3a0c12363ce12f9b71cbf5cc5b27bd80753edb2
Miguel <mgarcia95951@gmail.com>
2019-01-19 12:50:12 -0600

    set up reducer statement

--- src/components/RoomNavbar/RoomNavbar.jsx
+++ src/components/RoomNavbar/RoomNavbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Sidebar, Menu} from 'semantic-ui-react';
 
 class RoomNavbar extends React.Component {
   render() {
     return (
-      <div>
-        navbar
-      </div>
+      <Sidebar 
+        width='very thin'
+        icon='labeled'
+        as={Menu}
+        inverted
+        vertical
+        visible
+      >
+      </Sidebar>
     )
   }
 }
