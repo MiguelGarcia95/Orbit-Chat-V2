52edec26ab2c2b4dc51d56f2368bd4712192f612
Miguel <mgarcia95951@gmail.com>
2019-01-19 12:09:58 -0600

    set up reducer statement

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -21,7 +21,22 @@ const authReducer = (state = initialState, action) => {
     case actionTypes.LOGIN:
       return {
         ...state
-        
+      }
+    case actionTypes.LOGIN_ERROR:
+      return {
+        ...state
+      }
+    case actionTypes.SET_USER:
+      return {
+        ...state
+      }
+    case actionTypes.UNSET_USER:
+      return {
+        ...state
+      }
+    case actionTypes.CLEAR_USER:
+      return {
+        ...state
       }
     default:
       return state
