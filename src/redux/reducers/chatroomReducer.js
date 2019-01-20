ad8cf95191f16d1fae171ca007e2296f9c4fda26
Miguel <mgarcia95951@gmail.com>
2019-01-20 17:28:34 -0600

    fetched chatrooms

--- src/redux/reducers/chatroomReducer.js
+++ src/redux/reducers/chatroomReducer.js
@@ -11,7 +11,31 @@ const chatroomReducer = (state = initialState, action) => {
   switch(action.type) {
     case actionTypes.CREATE_NEW_CHATROOM: 
       return {
-        
+        ...state,
+        chatError: action.payload.chatError,
+        isLoading: action.payload.isLoading,
+        currentChatroom: action.payload.currentChatroom
+      }
+    case actionTypes.CREATE_NEW_CHATROOM_ERROR: 
+      return {
+        ...state,
+        chatError: action.payload.chatError,
+        isLoading: action.payload.isLoading,
+        currentChatroom: action.payload.currentChatroom
+      }
+    case actionTypes.GET_CHATROOMS: 
+      return {
+        ...state,
+        chatError: action.payload.chatError,
+        isLoading: action.payload.isLoading,
+        chatrooms: action.payload.chatrooms
+      }
+    case actionTypes.GET_CHATROOMS_ERROR: 
+      return {
+        ...state,
+        chatError: action.payload.chatError,
+        isLoading: action.payload.isLoading,
+        chatrooms: action.payload.chatrooms
       }
     default:
       return state
