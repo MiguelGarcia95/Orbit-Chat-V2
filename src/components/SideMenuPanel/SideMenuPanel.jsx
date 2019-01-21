299c34d7f5b99e5ec729a0a351f58c49753d99e2
Miguel <mgarcia95951@gmail.com>
2019-01-21 11:23:11 -0600

    pass chatroom data

--- src/components/SideMenuPanel/SideMenuPanel.jsx
+++ src/components/SideMenuPanel/SideMenuPanel.jsx
@@ -12,8 +12,16 @@ class SideMenuPanel extends React.Component {
       >
         <Menu.Header 
           as='h3'
+          className='Header__header'
           content={chatroom && chatroom.chatroom.name}
           style={{textAlign: 'center', padding: '10px 0px', background: '#232323', color: 'white'}} 
+        />
+
+        <Menu.Header 
+          as='h3'
+          className='Header__footer'
+          content={chatroom && chatroom.chatroom.name}
+          style={{textAlign: 'center', padding: '10px 0px', background: '#232323', color: 'white', position: 'absolute', bottom: '0', width: '100%', left: '0'}}
         />
       </Menu>
     )
