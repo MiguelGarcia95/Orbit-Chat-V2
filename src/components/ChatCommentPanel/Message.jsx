7366940ac38ff58a7635f72fa90687f53f7dd2cd
Miguel <mgarcia95951@gmail.com>
2019-01-25 19:55:32 -0600

    style comments

--- src/components/ChatCommentPanel/Message.jsx
+++ src/components/ChatCommentPanel/Message.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Comment} from 'semantic-ui-react';
+import moment from 'moment';
+
+const timeFromNow = timestamp => moment(timestamp).fromNow();
 
 const Message = ({message}) => {
   return (
@@ -7,7 +10,7 @@ const Message = ({message}) => {
       <Comment.Avatar src={message.avatar} />
       <Comment.Content>
         <Comment.Author as='a'>{message.username}</Comment.Author>
-        {/* <Comment.Metadata></Comment.Metadata> */}
+        <Comment.Metadata >{timeFromNow(message.createdDate.toDate())}</Comment.Metadata>
         <Comment.Text>{message.message}</Comment.Text>
       </Comment.Content>
     </Comment>
